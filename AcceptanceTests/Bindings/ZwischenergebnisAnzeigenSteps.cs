54db520774c675f33da2a9be1c19b9c7b7caa17d
suth <daniel.suth@iis.fraunhofer.de>
2020-10-07 12:12:00 +0200

    Zwischenergebnis anzeigen.

---
+++ AcceptanceTests/Bindings/ZwischenergebnisAnzeigenSteps.cs
@@ -0,0 +1,39 @@
+﻿using AkzeptanzTests;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.Features
+{
+    [Binding]
+    public class ZwischenergebnisAnzeigenSteps
+    {
+        private readonly TrackerDriver _driver;
+
+        public ZwischenergebnisAnzeigenSteps(TrackerDriver driver)
+        {
+            _driver = driver;
+        }
+
+        [When(@"ich Zwischenergebnis aufrufe")]
+        public void WhenIchZwischenergebnisAufrufe()
+        {
+            _driver.EmpfangeAnweisung("Zwischenergebnis");
+        }
+        
+
+        [Then(@"zeigt der NerdGolfTracker als Zwischenergebnis (.*) Schlag")]
+        public void ThenZeigtDerNerdGolfTrackerAlsZwischenergebnisSchlag(int p0)
+        {
+            _driver.AssertThatAntwortContains("{0}", p0);
+            _driver.AssertThatAntwortContains("Zwischenergebnis");
+        }
+
+        [Then(@"zeigt er als Zwischenergebnis (.*) Schlaege")]
+        public void ThenZeigtErAlsZwischenergebnisSchlaege(int p0)
+        {
+            _driver.AssertThatAntwortContains("{0}", p0);
+            _driver.AssertThatAntwortContains("Zwischenergebnis");
+        }
+
+    }
+}
---
+++ AcceptanceTests/Features/Zwischenergebnis anzeigen.feature
@@ -0,0 +1,19 @@
+﻿Feature: Zwischenergebnis anzeigen
+
+Scenario: Zwischenergebnisse anzeigen
+	When ich den Ball einmal schlage
+	And ich Zwischenergebnis aufrufe
+	Then zeigt der NerdGolfTracker als Zwischenergebnis 1 Schlag
+	
+	When ich den Ball noch einmal schlage
+	And ich Zwischenergebnis aufrufe
+	Then zeigt er als Zwischenergebnis 2 Schlaege
+
+	When ich zum naechsten Loch gehe
+	And ich Zwischenergebnis aufrufe
+	Then zeigt er als Zwischenergebnis 2 Schlaege
+
+	When ich den Ball noch einmal schlage
+	And ich Zwischenergebnis aufrufe
+	Then zeigt er als Zwischenergebnis 3 Schlaege
+
