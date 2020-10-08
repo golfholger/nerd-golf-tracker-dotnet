665d1751c76d95f7254e1bc75603ef0e6e9ab3c5
Sebastian Kudla <sebastian.kudla@iis.fraunhofer.de>
2020-10-08 11:27:00 +0200

    Added unit test and KombiOperation

---
+++ NerdGolfTracker/Operationen/KombiOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdGolfTracker.Operationen
+{
+	public class KombiOperation : Operation
+	{
+		private Operation[] _operationen;
+
+		public KombiOperation(params Operation[] operationen)
+		{
+			_operationen = operationen;
+		}
+
+		public string FuehreAus(Scorecard scorecard)
+		{
+			string result = "";
+
+			foreach(Operation operation in _operationen)
+			{
+				result += operation.FuehreAus(scorecard);
+			}
+
+			return result;
+		}
+	}
+}
