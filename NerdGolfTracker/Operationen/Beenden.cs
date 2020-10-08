dbc2c3868f060f53ff8b7ed229b4c862836a8524
Maik Luxa <maik.luxa@iis.fraunhofer.de>
2020-10-08 11:33:09 +0200

    Beenden Operation überarbeitet

--- NerdGolfTracker/Operationen/Beenden.cs
+++ NerdGolfTracker/Operationen/Beenden.cs
@@ -7,6 +7,7 @@ namespace NerdGolfTracker.Operationen
     {
         public string FuehreAus(Scorecard scorecard)
         {
+            scorecard.SchliesseSpielAb();
             return "Beende Anwendung";
         }
 
